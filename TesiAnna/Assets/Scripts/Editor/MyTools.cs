8e267997cea23dc9277d3643ead07c97ebc8856b
Anna Sansoè <annasansoe96@gmail.com>
2023-07-14 12:35:46 +0200

    CSV Files for my app

---
+++ TesiAnna/Assets/Scripts/Editor/MyTools.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor;
+
+public static class MyTools
+{
+    [MenuItem("MyTools/Add to Report %F1")]
+    static void DEV_AppendToReport()
+    {
+        CSVManager.AppendToReport(
+            new string[5]
+            {
+                "jonny",
+                Random.Range(0,11).ToString(),
+                Random.Range(0,11).ToString(),
+                Random.Range(0,11).ToString(),
+                Random.Range(0,11).ToString()
+            });
+        EditorApplication.Beep();
+    }
+
+    [MenuItem("MyTools/Reset Report %F12")]
+    static void DEV_ResetToReport()
+    {
+        CSVManager.CreateReport();
+
+        EditorApplication.Beep();
+    }
+}
