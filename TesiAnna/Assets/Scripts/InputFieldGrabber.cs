cca6220f2e29c4e56b01acfde8601a644340a32a
Annasansoe <72860993+Annasansoe@users.noreply.github.com>
2023-08-24 19:35:03 +0200

    Blinking caret working & works also the questionnaire
    The only things that doesn't work is the clear of input after press enter

--- TesiAnna/Assets/Scripts/InputFieldGrabber.cs
+++ TesiAnna/Assets/Scripts/InputFieldGrabber.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+
 
 public class InputFieldGrabber : MonoBehaviour
 {
@@ -12,27 +14,92 @@ public class InputFieldGrabber : MonoBehaviour
     [SerializeField] private GameObject reactiongGroup;
     [SerializeField] private TMP_Text reactionTextBox;
 
-    [Header("Parsed into int and float")]
-    [SerializeField] private int inputParsedAsInt;
-    [SerializeField] private float inputParsedAsFloat;
+    [Header("Grab from input and see the result")]
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] TMP_Text resultText;
 
-    public void GrabFromInputField (string input)
+    [Header("Question for test")]
+    [SerializeField] public GameObject questionPanel;
+    [SerializeField] public TMP_Text questionText;
+    
+    //public Button submitButton;
+
+    public List<Question> questions;
+    public int currentQuestionIndex = 0;
+
+    private void Start()
     {
-        if(input == string.Empty)
-        {
-            return;
-        }
-        inputText = input;
+        questions = new List<Question>
+    {
+        new Question("What is the capital of France?", "Paris"),
+        new Question("How many planets are there in our solar system?", "eight"),
+        new Question("How many days are there in a week?", "Seven"),
+        // ... Add more questions here with their respective expected answers
+    };
 
-        inputParsedAsInt = int.Parse(input);
-        inputParsedAsFloat = float.Parse(input);
-
-        DisplayReactionToInput();
+        DisplayQuestion(currentQuestionIndex);
     }
 
-    private void DisplayReactionToInput()
+    public void DisplayQuestion(int index)
     {
-        reactionTextBox.text = "Welcome to the team, " + inputText + "!";
-        reactiongGroup.SetActive(true);
+        questionText.text = questions[index].questionText;
+        inputText = "";
+    }
+   
+    public void OnSubmitAnswer()
+    {
+        string userAnswer = inputField.text.ToString();
+
+        if (userAnswer.ToLower() != questions[currentQuestionIndex].expectedAnswer.ToLower())
+        {
+            Debug.Log("Answer is incorrect!");
+            resultText.text = "Invalid input";
+            resultText.color = Color.red;
+           
+        }
+        else
+        {
+            Debug.Log("Answer is correct.");
+            resultText.text = "Valid Input";
+            resultText.color = Color.green;
+            // Move to the next question and display it
+            currentQuestionIndex++;
+        }
+        reactionTextBox.text = "Welcome to the team, " + userAnswer + "!";
+       
+        
+        if (currentQuestionIndex < questions.Count)
+        {
+            DisplayQuestion(currentQuestionIndex);
+        }
+        else
+        {
+            // No more questions
+            //questionPanel.SetActive(false);
+            questionText.text = "Questionnaire completed!";
+            Debug.Log("Questionnaire completed!");
+        }
     }
 }
+
+[System.Serializable]
+public class Question
+{
+    public string questionText;
+    public string expectedAnswer;
+
+    public Question(string text, string answer)
+    {
+        questionText = text;
+        expectedAnswer = answer;
+    }
+}
+
+
+
+
+
+
+
+
+
