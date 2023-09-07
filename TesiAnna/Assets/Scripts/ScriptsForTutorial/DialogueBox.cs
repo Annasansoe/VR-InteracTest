7a42a5598d09711f7541073d3a32f72433ed078a
Annasansoe <72860993+Annasansoe@users.noreply.github.com>
2023-09-07 22:41:11 +0200

    Making the tutorial scene one interactive

--- TesiAnna/Assets/Scripts/ScriptsForTutorial/DialogueBox.cs
+++ TesiAnna/Assets/Scripts/ScriptsForTutorial/DialogueBox.cs
@@ -9,64 +9,129 @@ public class DialogueBox : MonoBehaviour
 {
     public DialogueSegment[] DialogueSegments;
     [Space]
-    public XRGrabInteractable[] XRGrabInteractable;
+    public XRGrabInteractable XRGrabInteractable;
+    public GameObject Bin;
     public Button SkipIndicator;
     public TMP_Text DialogueDisplay;
     public Button GoToSceneOne;
-    public TMP_Text CollectedObjects;
+    public TMP_Text VerifyIsGrabbed;
+    public AudioSource DidIt;
+    public AudioSource binAudioSource;
+
     [Space]
     public float TextSpeed;
 
     private bool buttonClicked = false;
 
     private bool CanContinue;
+    public bool cubeIsGrabbed = false;
+    public bool cubeIsInTheBin = false;
     private int DialogueIndex;
+
+    static int totScore = 0;
+    static int totGrab = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(PlayDialogue(DialogueSegments[0].Dialogue));
+        DidIt.gameObject.SetActive(false);
+        //binAudioSource.gameObject.SetActive(false);
         GoToSceneOne.gameObject.SetActive(false);
-        CollectedObjects.gameObject.SetActive(false);
-        foreach(XRGrabInteractable interactable in XRGrabInteractable)
-        {
-            GameObject interactableObject = interactable.gameObject;
-            // Now you can work with the 'interactableObject'
-            interactableObject.gameObject.SetActive(false);
-        }
+        VerifyIsGrabbed.gameObject.SetActive(false);
+        Bin.gameObject.SetActive(false);
+        // Now you can work with the 'interactableObject'
+        XRGrabInteractable.gameObject.SetActive(false);
+        
     }
     public void Click()
     {
         buttonClicked = true;
+        //CollectedObjects.gameObject.SetActive(true);
+    }
+
+    public void isGrabbed()
+    {
+        VerifyIsGrabbed.gameObject.SetActive(true);
+        totGrab += 1;
+    }
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.CompareTag("Unsorted Waste"))
+        {
+            if (binAudioSource != null && binAudioSource.clip != null)
+            {
+                binAudioSource.Play();
+            }
+            totScore++;
+        }
+        Destroy(otherCollider.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         SkipIndicator.enabled = CanContinue;
-        if (buttonClicked && CanContinue)
+        
+        if (DialogueIndex != 1 && buttonClicked && CanContinue)
         {
             buttonClicked = false;
             DialogueIndex++;
+
             if (DialogueIndex == DialogueSegments.Length)
             {
                 gameObject.SetActive(false);
                 DialogueDisplay.gameObject.SetActive(false);
 
-                GoToSceneOne.gameObject.SetActive(true);
-                CollectedObjects.gameObject.SetActive(true);
-                foreach (XRGrabInteractable interactable in XRGrabInteractable)
-                {
-                    GameObject interactableObject = interactable.gameObject;
-                    // Now you can work with the 'interactableObject'
-                    interactableObject.gameObject.SetActive(true);
-                }
+                GoToSceneOne.gameObject.SetActive(true);               
             }
             StartCoroutine(PlayDialogue(DialogueSegments[DialogueIndex].Dialogue));
         }
-    }
+        else if(DialogueIndex == 2 )
+        {
+            SkipIndicator.enabled = false;
+            Bin.gameObject.SetActive(true);
+            VerifyIsGrabbed.gameObject.SetActive(false);
+            //VerifyIsGrabbed.text = "Tot score " + totScore.ToString();
+            if (totScore == 2)
+            {
+                VerifyIsGrabbed.gameObject.SetActive(true);
+                VerifyIsGrabbed.text = "Awesome you did it!";
+                SkipIndicator.enabled = true;
+               // DidIt.Play();
 
-    
+                if (buttonClicked && CanContinue ) 
+                {
+                    buttonClicked = false;
+                    DialogueIndex++;
+                    StartCoroutine(PlayDialogue(DialogueSegments[DialogueIndex].Dialogue));
+                    VerifyIsGrabbed.gameObject.SetActive(false);
+                }
+            }
+        }
+        else if (DialogueIndex == 1)
+        {
+            SkipIndicator.enabled = false;
+            XRGrabInteractable.gameObject.SetActive(true);
+
+            if (totGrab > 0)
+            {
+                VerifyIsGrabbed.text = "Great job!";
+                SkipIndicator.enabled = true;
+                DidIt.gameObject.SetActive(true);
+
+                if (buttonClicked && CanContinue)
+                {
+                    DidIt.gameObject.SetActive(false);
+                    buttonClicked = false;
+                    DialogueIndex++;
+                    StartCoroutine(PlayDialogue(DialogueSegments[DialogueIndex].Dialogue));
+                    VerifyIsGrabbed.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
 
     IEnumerator PlayDialogue(string Dialogue)
     {
